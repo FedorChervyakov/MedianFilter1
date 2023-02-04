e90395ed40d4e16b297ecd5d18aa3221c33f930a
Fedor Chervyakov <fchervyakov@gmail.com>
2023-02-04 11:45:07 +0100

    test: add more filtering tests

--- tests/test_median_filter.cpp
+++ tests/test_median_filter.cpp
@@ -2,6 +2,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+using namespace std;
+
 
 TEST(MedianFilterTests, TestReset) {
   constexpr int N_TAPS = 20;
@@ -19,22 +21,47 @@ TEST(MedianFilterTests, TestGetTaps) {
   ASSERT_EQ(actual, expected);
 }
 
-TEST(MedianFilterTests, TestFilterOperation_Const) {
-  constexpr int N_TAPS = 3;
-  constexpr int INPUT_SIZE = 5;
+class MedianFilterFilteringTests:
+  public testing::TestWithParam<tuple<int, vector<double>, vector<double>>>
+{
+};
 
-  constexpr double x[INPUT_SIZE] = {1, 1, 1, 1, 1};
-  constexpr double expected_y[INPUT_SIZE] = {0, 1, 1, 1, 1};
-  double actual_y[INPUT_SIZE] = {0};
+TEST_P(MedianFilterFilteringTests, FilterOperation) {
+  const int N_TAPS = get<0>(GetParam());
+  vector<double> input_x = get<1>(GetParam());
+  vector<double> expected_y = get<2>(GetParam());
+
+  vector<double> actual_y(expected_y.size(), 0);
 
   MedianFilter filter(N_TAPS);
 
-  for (auto i = 0; i<INPUT_SIZE; i++) {
-    actual_y[i] = filter.filter(x[i]);
+  for (int i = 0; i<expected_y.size(); i++) {
+    actual_y[i] = filter.filter(input_x[i]);
   }
 
   EXPECT_THAT(actual_y, testing::Pointwise(testing::DoubleEq(), expected_y));
 }
+
+const vector<tuple<int, vector<double>, vector<double>>> filtering_sequences {
+  // Test single tap median filter doesn't modify input
+  {1, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}},
+  {1, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}},
+  // Test double tap median filter produces halved values
+  {2, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}},
+  {2, {1, 2, 3, 4, 5}, {0.5, 1.5, 2.5, 3.5, 4.5}},
+  // Test tripple tap median filter filters correctly
+  {3, {1, 1, 1, 1, 1}, {0, 1, 1, 1, 1}},
+  {3, {-1, -1, -1, -1, -1}, {0, -1, -1, -1, -1}},
+  {3, {1, 2, 3, 4, 5}, {0, 1, 2, 3, 4}},
+  {3, {-2, -1, 0, 1, 2}, {0, -1, -1, 0, 1}},
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    MedianFilterTests,
+    MedianFilterFilteringTests,
+    ::testing::ValuesIn(filtering_sequences)
+);
+
 
 int main(int argc, char** argv)
 {
